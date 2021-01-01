74e451a04e6c3058b56daab8cd4b5a9e24708024
efemevlut <efemevlut20@gmail.com>
2021-01-01 23:32:41 +0300

    Create Redux structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 import share from "./images/share.png";
 import Posts from "./components/Posts/Posts";
 import Form from "./components/Form/Form";
 import useStyles from './App.styles'
+import {useDispatch} from 'react-redux';
+import {getPosts} from './actions/posts';
 
 function App() {
 
   const classes = useStyles();
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    dispatch(getPosts());
+  },[dispatch]);
 
   return (
     <Container maxWidth="lg">
--- server/index.js
+++ server/index.js
@@ -16,7 +16,6 @@ app.listen(port, () => {
 });
 
 
-app.use("/posts", postRoutes);
 
 
 //database connect
@@ -25,3 +24,5 @@ dbConnect();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+app.use("/posts", postRoutes);
