e85e1b8ccfe13a70c4e9885308eafffcafdf2f15
efemevlut <efemevlut20@gmail.com>
2021-01-01 18:26:45 +0300

    Add createPost function

--- server/controllers/post.js
+++ server/controllers/post.js
@@ -1,4 +1,24 @@
-exports.getPosts = (req,res) => {
-    res.send("This Works");
-}
+const PostMessage = require("../models/postMessage");
 
+exports.getPosts = async (req, res) => {
+  try {
+    const postMessages = await PostMessage.find();
+    console.log(postMessages);
+    res.status(200).json(postMessages);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+exports.createPost = async (req, res) => {
+  const body = req.body;
+
+  const newPost = new PostMessage(post);
+
+  try {
+    await newPost.save();
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+};
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const {getPosts} = require("../controllers/post")
+const { getPosts, createPost } = require("../controllers/post");
 const router = express.Router();
-
 
 //http://localhost:5000/posts
 
 router.get("/", getPosts);
+router.post("/", createPost);
 
 module.exports = router;
