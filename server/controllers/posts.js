10b0119b9c447e3fc030528664f15e9fcb93056e
efemevlut <efemevlut20@gmail.com>
2021-01-04 19:52:58 +0300

    Add update post

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,9 +1,9 @@
+const mongoose = require("mongoose");
 const PostMessage = require("../models/postMessage");
 
 exports.getPosts = async (req, res) => {
   try {
     const postMessages = await PostMessage.find();
-    console.log(postMessages);
     res.status(200).json(postMessages);
   } catch (error) {
     res.status(404).json({ message: error.message });
@@ -22,3 +22,13 @@ exports.createPost = async (req, res) => {
     res.status(409).json({ message: error.message });
   }
 };
+
+exports.updatePost = async (req, res) => {
+  const {id: _id} = req.params;
+  const post = req.body;
+  if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send("No post with that id");
+
+  const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, {new:true});
+
+  res.json(updatedPost);
+}
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { getPosts, createPost } = require("../controllers/posts.js");
+const { getPosts, createPost, updatePost } = require("../controllers/posts.js");
 const router = express.Router();
 
 //http://localhost:5000/posts
 
 router.get("/", getPosts);
 router.post("/", createPost);
+router.patch("/:id", updatePost);
 
 module.exports = router;
