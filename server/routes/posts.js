5b201248a2bf63ca5375dc5b089a97e12762e557
efemevlut <efemevlut20@gmail.com>
2021-01-09 23:05:25 +0300

    Add like post to server side

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { getPosts, createPost, updatePost, deletePost } = require("../controllers/posts.js");
+const { getPosts, createPost, updatePost, deletePost, likePost } = require("../controllers/posts.js");
 const router = express.Router();
 
 //http://localhost:5000/posts
@@ -7,6 +7,7 @@ const router = express.Router();
 router.get("/", getPosts);
 router.post("/", createPost);
 router.patch("/:id", updatePost);
-router.delete("/:id",deletePost)
+router.delete("/:id",deletePost);
+router.patch("/:id/likePost", likePost);
 
 module.exports = router;
